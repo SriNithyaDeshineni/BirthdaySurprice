bbd5f47c4b69ecae08052d0da6d190fa51e5953a
SriNithyaDeshineni <srinithyadeshineni@gmail.com>
2026-08-20 19:01:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function sayYes() {
+
+    window.location.href = "pages/bday.html";
+
+}
+
+
+function sayNo() {
+
+    document.getElementById("questionBox").style.display = "none";
+
+    document.getElementById("noBox").classList.remove("hidden");
+
+}
+
+
+function goToBirthday() {
+
+    window.location.href = "pages/bday.html";
+
+}
